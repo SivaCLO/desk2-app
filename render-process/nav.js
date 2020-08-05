fdd7f1c5e2c675a268af458558035836a136a80d
Siva Ragavan <siva.ragavan@pm.me>
2020-08-05 10:28:06 -0400

    Updated primary button style and removed some menu options

--- render-process/nav.js
+++ render-process/nav.js
@@ -7,14 +7,5 @@ document.body.addEventListener('click', (event) => {
 })
 
 function handleAction (event) {
-  const buttons = document.querySelectorAll('.nav-button.is-selected')
-  Array.prototype.forEach.call(buttons, (button) => {
-    button.classList.remove('is-selected')
-  })
-
-  // Highlight clicked button and show view
-  event.target.classList.add('is-selected')
-
   // Update BrowserView to point to the right action
-
 }
