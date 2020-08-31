06fa495ea997a765e2b1cf05ba59b587ac9adc5f
Siva Ragavan <siva.ragavan@pm.me>
2020-08-31 06:16:21 -0400

    Using Siva's userId as the default

--- main-process/system/activity.js
+++ main-process/system/activity.js
@@ -4,7 +4,12 @@ function log(activityCode, activityData) {
   axios
     .post(
       "https://md-functions.azurewebsites.net/api/activity?code=Qv3qRgZB1ZbBLArwzBTrF08HssguuMh7tEMSUCP3Eeakl/sIQOaaIw==",
-      { userId: "test", activityCode, activityData, activityTime: Date.now() }
+      {
+        userId: "172949fd7b6203109290f0ba503057336fd72a5dd91b18858155183238fafe37f",
+        activityCode,
+        activityData,
+        activityTime: Date.now(),
+      }
     )
     .then((res) => {})
     .catch((err) => {
