39a604b1122d7d3c89d281f4b18db6c21670791c
Siva Ragavan <siva.ragavan@pm.me>
2020-08-31 06:12:12 -0400

    changed mediumId to userId

--- main-process/system/activity.js
+++ main-process/system/activity.js
@@ -4,7 +4,7 @@ function log(activityCode, activityData) {
   axios
     .post(
       "https://md-functions.azurewebsites.net/api/activity?code=Qv3qRgZB1ZbBLArwzBTrF08HssguuMh7tEMSUCP3Eeakl/sIQOaaIw==",
-      { mediumId: "test", activityCode, activityData, activityTime: Date.now() }
+      { userId: "test", activityCode, activityData, activityTime: Date.now() }
     )
     .then((res) => {})
     .catch((err) => {
