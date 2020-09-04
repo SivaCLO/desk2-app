a3d21b9d942f422d4789b9816bbf06d6c2bb594b
Siva Ragavan <siva.ragavan@pm.me>
2020-09-04 07:13:39 -0400

    Added logs for update callbacks

--- main-process/system/updater.js
+++ main-process/system/updater.js
@@ -7,18 +7,22 @@ function checkForUpdates() {
 }
 
 autoUpdater.on("checking-for-update", () => {
+  console.log("Checking for update");
   log("app/update/check");
 });
 
 autoUpdater.on("update-available", () => {
+  console.log("Update available");
   log("app/update/available");
 });
 
 autoUpdater.on("error", (err) => {
+  console.log("Error in updating app");
   log("app/update/error", err);
 });
 
 autoUpdater.on("update-downloaded", (info) => {
+  console.log("Update downloaded", info);
   log("app/update/downloaded", info);
 });
 
