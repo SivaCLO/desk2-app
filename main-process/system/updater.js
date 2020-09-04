0e478fca04cbb27e2e4c03919da4820c6b6fa4bf
Siva Ragavan <siva.ragavan@pm.me>
2020-09-04 07:11:37 -0400

    Removed all dialogs in update workflow to keep it smooth for now

--- main-process/system/updater.js
+++ main-process/system/updater.js
@@ -1,44 +1,25 @@
 const { autoUpdater } = require("electron-updater");
-const { dialog } = require("electron");
 const { log } = require("../../common/activity");
+autoUpdater.logger = require("electron-log");
 
 function checkForUpdates() {
   autoUpdater.checkForUpdates().then();
 }
 
-autoUpdater.logger = require("electron-log");
-
 autoUpdater.on("checking-for-update", () => {
-  showUpdateMessage("Checking for update...");
+  log("app/update/check");
 });
 
 autoUpdater.on("update-available", () => {
-  showUpdateMessage("Update available.");
-});
-
-autoUpdater.on("update-not-available", () => {
-  showUpdateMessage("Update not available.");
+  log("app/update/available");
 });
 
 autoUpdater.on("error", (err) => {
-  showUpdateMessage("Error in auto-updater. " + err);
   log("app/update/error", err);
 });
 
 autoUpdater.on("update-downloaded", (info) => {
-  showUpdateMessage("Update downloaded");
   log("app/update/downloaded", info);
 });
-
-function showUpdateMessage(message) {
-  dialog
-    .showMessageBox(null, {
-      title: "Update  status",
-      buttons: ["OK"],
-      type: "warning",
-      message,
-    })
-    .then();
-}
 
 module.exports = { checkForUpdates };
