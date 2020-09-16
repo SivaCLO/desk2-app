58abd8ff80b12329821a3af5704527b07df5e64a
Siva Ragavan <siva.ragavan@pm.me>
2020-09-16 18:17:45 -0400

    Added log for External link opens

--- render-process/ex-links.js
+++ render-process/ex-links.js
@@ -1,13 +1,15 @@
-const shell = require('electron').shell
+const shell = require("electron").shell;
+const { log } = require("../common/activity");
 
-const links = document.querySelectorAll('a[href]')
+const links = document.querySelectorAll("a[href]");
 
 Array.prototype.forEach.call(links, (link) => {
-  const url = link.getAttribute('href')
-  if (url.indexOf('http') === 0) {
-    link.addEventListener('click', (e) => {
-      e.preventDefault()
-      shell.openExternal(url)
-    })
+  const url = link.getAttribute("href");
+  if (url.indexOf("http") === 0) {
+    link.addEventListener("click", (e) => {
+      log("ex-links/open", url);
+      e.preventDefault();
+      shell.openExternal(url);
+    });
   }
-})
+});
