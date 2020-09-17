7578fe02954eeb0684ff87abfbc63fc52933335a
Sivaprakash Ragavan <siva@Sivaprakashs-MacBook-Pro.local>
2020-09-16 22:53:25 -0400

    Fixing a typo

--- render-process/quicklinks/quicklinks.js
+++ render-process/quicklinks/quicklinks.js
@@ -9,18 +9,18 @@ document.body.addEventListener("click", (event) => {
 
 function handleAction(event) {
   let action = event.target.dataset.action;
-  if (action === "stories") {
-    log("quicklinks/stories");
-    ipcRenderer.send("open-quicklink", "https://medium.com/me/stories/public");
+  if (action === "stats") {
+    log("quicklinks/stats");
+    ipcRenderer.send("open-quicklink", "https://medium.com/me/stats");
   } else if (action === "profile") {
     log("quicklinks/profile");
     ipcRenderer.send("open-quicklink", "https://medium.com/me");
   } else if (action === "settings") {
     log("quicklinks/settings");
     ipcRenderer.send("open-quicklink", "https://medium.com/me/settings");
-  } else if (action === "series") {
-    log("quicklinks/series");
-    ipcRenderer.send("open-quicklink", "https://medium.com/me/series/drafts");
+  } else if (action === "stories") {
+    log("quicklinks/stories");
+    ipcRenderer.send("open-quicklink", "https://medium.com/me/stories/public");
   } else if (action === "cancel") {
     log("quicklinks/cancel");
   }
