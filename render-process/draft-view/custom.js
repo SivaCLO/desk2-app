1f6069a79c789af9ef0f8bc6ffed085ea1cd6072
SurenRavikumar <suren@clovecrypto.com>
2020-09-17 13:13:18 +0530

    updated the function name and function call create 0 ms and 1500 ms

--- render-process/draft-view/custom.js
+++ render-process/draft-view/custom.js
@@ -1,41 +1,46 @@
 console.log("Custom JS File loaded successfully...");
 
 window.addEventListener("load", () => {
-  hider();
-  dividerAndPublicationHider();
+  hideMediumLogoAndAvatar();
+  hideDividerAndPublicationLogo();
   document.body.addEventListener("click", (event) => {
-    // console.log("Dataset action events", event.target.dataset.action);
+    console.log("Dataset action events", event.target.dataset.action);
     if (
       event.target.dataset.action === "save-draft-collection" ||
       event.target.dataset.action === "overlay-confirm" ||
       event.target.dataset.action === "close"
     ) {
-      hider();
+      hideMediumLogoAndAvatar();
+      setTimeout(() => {
+        hideMediumLogoAndAvatar();
+      }, 1500);
     }
     if (event.target.dataset.action === "add-to-collection") {
-      dividerAndPublicationHider();
+      hideDividerAndPublicationLogo();
+      setTimeout(() => {
+        hideDividerAndPublicationLogo();
+      }, 1500);
     }
   });
 });
 
-function hider() {
-  setTimeout(() => {
-    if (document.getElementsByClassName("js-metabarLogoLeft").length > 0)
-      document.getElementsByClassName("js-metabarLogoLeft").item(0).style.display = "none";
-    if (document.getElementsByClassName("buttonSet buttonSet--wide").length > 0)
-      document.getElementsByClassName("buttonSet buttonSet--wide").item(0).style.display = "none";
-  }, 1500);
+function hideMediumLogoAndAvatar() {
+  console.log("hideMediumLogoAndAvatar");
+  if (document.getElementsByClassName("js-metabarLogoLeft").length > 0)
+    document.getElementsByClassName("js-metabarLogoLeft").item(0).style.display = "none";
+  if (document.getElementsByClassName("buttonSet buttonSet--wide").length > 0)
+    document.getElementsByClassName("buttonSet buttonSet--wide").item(0).style.display = "none";
 }
 
-function dividerAndPublicationHider() {
-  setTimeout(() => {
-    let metaBarLeft = document.getElementsByClassName("metabar-block").item(0).childNodes;
-    if (metaBarLeft.length > 3) {
-      metaBarLeft[0].innerHTML = "";
-      metaBarLeft[1].innerHTML = "";
-      metaBarLeft[2].innerHTML = "";
-      metaBarLeft[3].innerHTML = "";
-    }
-  }, 1000);
-  hider();
+function hideDividerAndPublicationLogo() {
+  console.log("hideDividerAndPublicationLogo");
+  let metaBarLeft = document.getElementsByClassName("metabar-block").item(0).childNodes;
+  if (metaBarLeft.length > 3) {
+    metaBarLeft[0].innerHTML = "";
+    metaBarLeft[1].innerHTML = "";
+    metaBarLeft[2].innerHTML = "";
+    metaBarLeft[3].innerHTML = "";
+  }
+
+  hideMediumLogoAndAvatar();
 }
