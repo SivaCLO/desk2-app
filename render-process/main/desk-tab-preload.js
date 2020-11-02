910888c7f5e2a7d8b3dd1b334d238243135892a1
SurenRavikumar <suren@clovecrypto.com>
2020-11-02 12:16:15 +0530

    removed the unwanted lines

--- render-process/main/desk-tab-preload.js
+++ render-process/main/desk-tab-preload.js
@@ -1,13 +1,11 @@
 console.log("Desk Preload Executing Successfully.");
 
-const { deskSignout } = require("../../common/desk");
 const { ipcRenderer } = require("electron");
 
 window.addEventListener("load", () => {
   document.body.addEventListener("click", (event) => {
     if (event.target.innerText === "Sign out") {
       ipcRenderer.send("signOut");
-      deskSignout();
     }
   });
 });
