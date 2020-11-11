54f8020946b09d678d7ae0d4f681e8b7c50a808e
SurenRavikumar <suren@clovecrypto.com>
2020-11-11 15:44:34 +0530

    stage 2 search option updated

--- render-process/mediabrowser/mediabrowser.js
+++ render-process/mediabrowser/mediabrowser.js
@@ -2,15 +2,31 @@ console.log("mediabrowser.js Executed Successfully...!");
 const axios = require("axios");
 const { ipcRenderer } = require("electron");
 
-document.getElementById("giphy-search").addEventListener("click", async (event) => {
-  var searchValue = document.getElementById("searchBox").value;
-  if (searchValue === "" || searchValue === undefined) {
-    document.getElementById("searchMessage").hidden = false;
+document.getElementById("mediaItems").addEventListener("click", (event) => {
+  //   console.log("event", event);
+  document.getElementById("search").hidden = false;
+  if (event.target.parentElement.id === "gif") document.getElementById("searchTitle").innerText = "Search GIF";
+  if (event.target.parentElement.id === "screenshot")
+    document.getElementById("searchTitle").innerText = "Search Screenshot";
+  if (event.target.parentElement.id === "image") document.getElementById("searchTitle").innerText = "Search Image";
+  if (event.target.parentElement.id === "video") document.getElementById("searchTitle").innerText = "Search Video";
+  if (event.target.parentElement.id === "audio") document.getElementById("searchTitle").innerText = "Search Audio";
+});
+
+document.getElementById("searchBox").addEventListener("keyup", function () {
+  if (document.getElementById("searchBox").value === "") {
+    document.getElementById("media-search-button").disabled = true;
   } else {
-    var data = await giphy(searchValue);
+    document.getElementById("media-search-button").disabled = false;
+  }
+});
+
+document.getElementById("media-search-button").addEventListener("click", async (event) => {
+  if (document.getElementById("searchTitle").innerText === "Search GIF") {
+    var data = await giphy(document.getElementById("searchBox").value);
     data.data.map((index) => {
       var span = document.createElement("span");
-      span.setAttribute("class", "col-md-2 mt-3");
+      span.setAttribute("class", "col-md-2 mt-1");
       var img = document.createElement("img");
       img.setAttribute("class", "img-fluid rounded");
       img.setAttribute("src", index.images.fixed_height.url);
@@ -23,6 +39,19 @@ document.getElementById("giphy-search").addEventListener("click", async (event) 
     elem.addEventListener("contextmenu", (event) => {
       ipcRenderer.send("insertGif", { id: event.srcElement.id });
     });
+  }
+
+  if (document.getElementById("searchTitle").innerText === "Search Screenshot") {
+    console.log(document.getElementById("searchTitle").innerText);
+  }
+  if (document.getElementById("searchTitle").innerText === "Search Image") {
+    console.log(document.getElementById("searchTitle").innerText);
+  }
+  if (document.getElementById("searchTitle").innerText === "Search Video") {
+    console.log(document.getElementById("searchTitle").innerText);
+  }
+  if (document.getElementById("searchTitle").innerText === "Search Audio") {
+    console.log(document.getElementById("searchTitle").innerText);
   }
 });
 
